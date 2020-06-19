e3f116d9789844e15126c4df0c0627ec248a20c7
datingstrings <connect@datingstrings.com>
2020-06-19 18:44:28 +0530

    changes in mail server settings

--- config/mail.php
+++ config/mail.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'driver' => 'smtp',
 
     /*
     |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => 'smtpout.secureserver.net',
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => 'saurabh@nemateacompany.com',
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => 'Funbase1#',
 
     'pretend' => false,
 
