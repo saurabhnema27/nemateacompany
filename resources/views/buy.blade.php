5d968bf045dd2470c02bb4916cd99e8939753718
datingstrings <connect@datingstrings.com>
2020-06-26 13:57:56 +0530

    changes on buy blade

--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -82,10 +82,12 @@
                                 </div>
                                 <hr>
                                 <h3 class="h4">Order Details</h3>
+                                <br>
                                 <div class="form-row">
                                 <div class="form-group col-md-4">
                                     <label>Product Type</label>
                                     <select class="form-control summable" id="order_det" name="product_type" required = "required">
+                                        <option selected="selected">None</option>
                                         <option value="320">Tea Leaves</option>
                                         <option value="250">Granularity Tea</option>
                                         <option value="220">Dust Tea</option>
@@ -101,6 +103,22 @@
                                     <input type="radio" name="text" value="cod" checked="">
                                   </label>
                                 </div>
+                                </div>
+                                <div class="card" id="prodectoverview1">
+                                  <div class="card-header">
+                                    Prodect Overview
+                                  </div>
+                                  <div class="card-body">
+                                    <div class="row">
+                                      <div class="col-md">
+                                        <span><img id = "image" class = "rounded border border-light" src="/images/tea3.jpg" height="129" width="222"/></span>
+                                      </div>
+                                      <div class="col-md">
+                                        <h5 class="card-title mt-3" id="card-title"></h5>
+                                        <p class="card-text" id="card-content"></p>
+                                      </div>    
+                                    </div>
+                                  </div>
                                 </div>
                                 <label for="InputPrice">Price (in INR)</label>
                                 <input class="form-control col-md-12" id = "sum" type="text" name="price" readonly>
@@ -126,6 +144,7 @@
                 document.getElementById('sum').value=c;
             }
 
+            // for displaying the time messages
             var myDate = new Date();
             var hrs = myDate.getHours();
 
@@ -139,5 +158,37 @@
                 greet = 'Good Evening';
 
             document.getElementById('lblGreetings').innerHTML = greet;
+
+            $(document).ready(function() {
+            $('#order_det').on('change', function() {
+            $('#prodectoverview1').hide();
+            if ( this.value === '320') 
+            {
+              document.getElementById('image').src = "/images/tealeaves.png";
+              document.getElementById('card-title').innerHTML = "Tea Leaves";
+              document.getElementById('card-content').innerHTML = "This type of Tea leave will only give you a great taste but not good color.";
+              $("#prodectoverview1").show();
+            }
+            else if(this.value === '250')
+            {
+              document.getElementById('image').src = "/images/granual.jpg";
+              document.getElementById('card-title').innerHTML = "Granularity Tea";
+              document.getElementById('card-content').innerHTML = " <strong> This is our 2 Best Seller Tea,</strong> It give you a light color but a great taste in tea";
+              $("#prodectoverview1").show();
+            }
+            else if(this.value === '220')
+            {
+              document.getElementById('image').src = "/images/dust.jpg";
+              document.getElementById('card-title').innerHTML = "Dust Tea";
+              document.getElementById('card-content').innerHTML = "* <strong> This is our Best Seller Tea,</strong> It is mainly use to get a color in tea with a light taste in it.";
+              $("#prodectoverview1").show();
+            }
+            else
+            {
+                $("#prodectoverview1").hide();
+            }
+          })
+            .change();
+          });
         </script>
 @endsection
